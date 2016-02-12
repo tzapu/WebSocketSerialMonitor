2bc463a48c39e70ffdc601985b5f2746a49506cc
Alex Tudorache <alex@tzapu.com>
2016-02-12 17:33:49 +0200

    first build

---
+++ WebSocketSerialMonitor.ino
@@ -0,0 +1,101 @@
+/*
+   WebSocketServer.ino
+
+    Created on: 22.05.2015
+
+*/
+
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <WebSocketsServer.h>   //https://github.com/Links2004/arduinoWebSockets/tree/async
+#include <Hash.h>
+
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>        //https://github.com/tzapu/WiFiManager
+
+WebSocketsServer webSocket = WebSocketsServer(81);
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght) {
+
+  switch (type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[%u] Disconnected!\n", num);
+      break;
+    case WStype_CONNECTED:
+      {
+        IPAddress ip = webSocket.remoteIP(num);
+        Serial.printf("[%u] Connected from %s url: %s\n", num, ip.toString().c_str(), payload);
+
+        // send message to client
+        webSocket.sendTXT(num, "Connected to Serial on " + ip.toString() + "\n");
+      }
+      break;
+    case WStype_TEXT:
+      Serial.printf("[%u] get Text: %s\n", num, payload);
+
+      // send message to client
+      // webSocket.sendTXT(num, "message here");
+
+      // send data to all connected clients
+      // webSocket.broadcastTXT("message here");
+      break;
+    case WStype_BIN:
+      Serial.printf("[%u] get binary lenght: %u\n", num, lenght);
+      hexdump(payload, lenght);
+
+      // send message to client
+      // webSocket.sendBIN(num, payload, lenght);
+      break;
+  }
+
+}
+
+void setup() {
+  // Serial.begin(921600);
+  Serial.begin(115200);
+
+  //Serial.setDebugOutput(true);
+
+  Serial.println();
+  Serial.println();
+  Serial.println();
+  
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
+
+  //reset settings - for testing
+  //wifiManager.resetSettings();
+
+
+  //tries to connect to last known settings
+  //if it does not connect it starts an access point with the specified name
+  //here  "AutoConnectAP" with password "password"
+  //and goes into a blocking loop awaiting configuration
+  if (!wifiManager.autoConnect("AutoConnectAP", "password")) {
+    Serial.println("failed to connect, we should reset as see if it connects");
+    delay(3000);
+    ESP.reset();
+    delay(5000);
+  }
+
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+
+  Serial.println("START MIRRORING SERIAL");
+}
+
+void loop() {
+  Serial.setTimeout(200);
+  String line = Serial.readStringUntil('\n');
+  if (line.length() > 0) {
+    // add back line ending
+    line += '\n';
+    webSocket.broadcastTXT(line);
+    Serial.print(line);
+  }
+  webSocket.loop();
+}
+
